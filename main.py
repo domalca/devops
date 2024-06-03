e85dabc932793ca7a1765128b7b257c6c8a9ec0f
domalca <93787735+domalca@users.noreply.github.com>
2024-06-03 16:09:25 +0200

    update from cftic

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hola mundito")
+if 5 > 2:
+  print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!") 
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+myvar = "John"
